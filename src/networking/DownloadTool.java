0d67d010f08a2dfd9bf1400eadf86c5ef715d2e5
Lofe <784893480@qq.com>
2018-02-20 14:00:28 +0800

    网络文件下载工具实现

---
+++ src/networking/DownloadTool.java
@@ -0,0 +1,52 @@
+package networking;
+
+import java.io.*;
+import java.net.URL;
+
+public class DownloadTool {
+
+    public static void download(String url, String saveDir, String fileName) {
+
+        BufferedOutputStream bos = null;
+        InputStream is = null;
+        try {
+            byte[] buff = new byte[8192];
+            is = new URL(url).openStream();
+            File file = new File(saveDir, fileName);
+            file.getParentFile().mkdirs();
+            bos = new BufferedOutputStream(new FileOutputStream(file));
+            int count = 0;
+            while ( (count = is.read(buff)) != -1) {
+                bos.write(buff, 0, count);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        DownloadTool.download("http://www.baidu.com/img/bd_logo1.png", "/home/panda/picture", "baidu_logo.png");
+        System.out.println("done...");
+    }
+
+}
